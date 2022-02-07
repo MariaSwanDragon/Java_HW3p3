9ce2b488ceb0cc22b2ae96ced31a2d541c6dec07
Mariia <lebedevamv1@gmail.com>
2022-02-07 11:18:47 +0300

    add changes

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double sum, double pyear, int month) {
-       double pmonth = pyear / 100 / 12;
-       double summonth = sum * (pmonth + pmonth / (Math.pow(1 + pmonth, month) - 1));
+        double pmonth = pyear / 100 / 12;
+        double summonth = sum * (pmonth + pmonth / (Math.pow(1 + pmonth, month) - 1));
 
         return summonth;
     }
